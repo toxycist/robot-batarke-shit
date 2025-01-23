dc03994f3f35a587603a803d35737af09ad2a143
toxie <gamepiatnitza@gmail.com>
2025-01-23 10:25:36 +0200

    einu est, tuoj grisiu(tikriausiai)

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const cors = require('cors');
+const fs = require('fs');
+const { create } = require('domain');
 
 const app = express();
 const port = 3000;
@@ -25,15 +27,6 @@ db.run('CREATE TABLE IF NOT EXISTS "batarkes" (numeris INTEGER NOT NULL, pakraut
 });
 
 app.get('/data', (req, res) => {
-    if (req.query.numeris) {
-        db.run('UPDATE batarkes SET pakrauta = ABS(pakrauta-1), laikas = current_timestamp WHERE numeris = ?', [numeris], function(err) {
-            if (err) {
-                res.status(500).json(err.message);
-            } else {
-                res.json();
-            }
-        });
-    }
     db.all('SELECT * FROM batarkes', (err, rows) => {
         if (err) {
             res.status(500).json(err.message);
@@ -45,6 +38,7 @@ app.get('/data', (req, res) => {
 
 app.post('/add', (req, res) => {
     const {numeris: numeris, pakrauta: pakrauta} = req.body;
+    createFile(numeris)
     db.run('INSERT INTO batarkes (numeris, pakrauta) VALUES (?, ?)', [numeris, pakrauta], function(err) {
         if (err) {
             res.status(500).json(err.message);
@@ -67,6 +61,7 @@ app.patch('/change', (req, res) => {
 
 app.delete('/delete', (req, res) => {
     const numeris = req.query.numeris;
+    deleteFile(numeris)
     db.run('DELETE FROM batarkes WHERE numeris=?', [numeris], function(err) {
         if (err) {
             res.status(500).json(err.message);
@@ -76,6 +71,26 @@ app.delete('/delete', (req, res) => {
     });
 });
 
+function createFile(numeris){
+    fs.writeFileSync(`${numeris}.html`, `<!DOCTYPE html>
+        <html lang="en">
+        <head></head>
+        <body>
+        <script>
+            const server_adress = "http://127.0.0.1:3000"
+            window.onload = (() =>fetch(server_adress + '/change?numeris=${numeris}', {method: 'PATCH'}).then(window.location.href="index.html"))
+        </script>
+        </body>
+        </html>`)
+};
+
+function deleteFile(numeris){
+    fs.unlink(`${numeris}.html`, ()=>{})
+}
+
 app.listen(port, () => {
+    db.all('SELECT * FROM batarkes', (err, rows) => {rows.forEach(row => {
+        createFile(row.numeris)
+    })})
     console.log(`Server running at http://127.0.0.1:${port}`);
 });
